bb405af019f696b72d01e06a9907c8e5d73dff07
Brendan S <sheehybrendan@aol.com>
2013-11-05 11:48:40 -0500

    add to list on enter key

--- todo.js
+++ todo.js
@@ -1,8 +1,4 @@
-
-
-$(function(){
-
-$(".addtolist").on("click",function(){
+function adder() {
 	var input = $("#thingtodo").val();
 	$('ul').append("<li><input class='check' type='checkbox'/><span>"+input+"</span></li>");
 	$("#thingtodo").val("");
@@ -10,7 +6,18 @@ $(".addtolist").on("click",function(){
 		$(this).siblings('span').toggleClass('strike');
 		}); //end input.check click function
 	$('#thingtodo').focus();
-	});
+	};
+
+
+$(function(){
+	$(".addtolist").click(adder);
+	$("#thingtodo").keypress(function(e){
+		if (e.keyCode == 13 || e.keyCode == 108){
+			adder();
+		}
+	})
+
+
 
 $("button.clear").on("click", function(){
 	$('span.strike').closest('li').remove();
@@ -18,9 +25,15 @@ $("button.clear").on("click", function(){
 });
 }); //end doc load
 
-
-
-
+// $(".addtolist").on("click",function(){
+// 	var input = $("#thingtodo").val();
+// 	$('ul').append("<li><input class='check' type='checkbox'/><span>"+input+"</span></li>");
+// 	$("#thingtodo").val("");
+// 		$('ul li:last-child').children("input.check").click(function(){
+// 		$(this).siblings('span').toggleClass('strike');
+// 		}); //end input.check click function
+// 	$('#thingtodo').focus();
+// 	});
 
 
 
